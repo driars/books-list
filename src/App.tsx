cba164d69afa4c0a9eaf7143db862ec71dac144f
driars <driars0329@gmail.com>
2023-09-22 08:29:58 +0200

    Add Redux & TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="flex items-center justify-center w-screen h-screen text-3xl font-bold underline text-red-600">
+            Simple React Typescript Tailwind Sample
+          </h1>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/store/slices/booksSlice.ts
@@ -0,0 +1,28 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { BooksState } from "../types";
+
+// Define the initial state using that type
+const initialState: BooksState = {
+  value: 0,
+};
+
+export const booksSlice = createSlice({
+  name: "books",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount } = booksSlice.actions;
+
+export default booksSlice.reducer;
