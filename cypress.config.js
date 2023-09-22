3dc7e14083c8511fa6980139d75f136133f8ae25
driars <driars0329@gmail.com>
2023-09-22 11:42:20 +0200

    Implement HomePage integration testing by Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-undef */
-module.exports = {
+import { defineConfig } from 'cypress';
+
+module.exports = defineConfig({
   // ...rest of the Cypress project config
   projectId: 'a9ejph',
 
@@ -8,5 +9,7 @@ module.exports = {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:3000/books-list',
+    specPattern: 'cypress/tests/**/*.spec.{js,jsx,ts,tsx}',
   },
-};
+});
